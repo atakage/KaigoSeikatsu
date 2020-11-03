1f90522b1df1df1e4b5a2e0c9966f7950cfc4e4b
atakoko <atakoko2@gmail.com>
2020-11-03 22:02:48 +0900

    add list shuffle

--- Assets/Scripts/StreetManager.cs
+++ Assets/Scripts/StreetManager.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using UnityEngine;
+
 
 public class StreetManager : MonoBehaviour
 {
@@ -10,9 +13,32 @@ public class StreetManager : MonoBehaviour
     public SceneTransitionManager sceneTransitionManager;
     public string textFileName;
     public string nextSceneName;
+    public bool buttonOrContactStart; 
+    public string buttonOrContact; // 次のパタンを決めるために
     
     private void Start()
-    { 
+    {
+        // パタン始まりの信号
+        if (buttonOrContactStart)
+        {
+            // ランダムでbuttonやcontactを決定
+            List<string> btnConArrayList = new List<string>(); // 7(button):3(contact)
+            btnConArrayList.Add("B");
+            btnConArrayList.Add("B");
+            btnConArrayList.Add("B");
+            btnConArrayList.Add("B");
+            btnConArrayList.Add("B");
+            btnConArrayList.Add("B");
+            btnConArrayList.Add("B");
+            btnConArrayList.Add("C");
+            btnConArrayList.Add("C");
+            btnConArrayList.Add("C");
+
+            List<string> shuffledbtnConArrayList = shuffleList(btnConArrayList);
+            string result = string.Join(",", shuffledbtnConArrayList);
+            Debug.Log(result);
+        }
+
         TextAsset textAsset = Resources.Load(textFileName, typeof(TextAsset)) as TextAsset;
         string[] dataArray = textAsset.text.Split('/');
         Debug.Log(dataArray.Length);
@@ -29,20 +55,42 @@ public class StreetManager : MonoBehaviour
 
             if (Input.GetMouseButtonDown(0))
             {
-                // スクリプトをすべて読んだら
-                if (index > dataArray.Length-1)
+                // スクリプトをすべて読んだら && 次のシーンがあったら
+                if (index > dataArray.Length-1 && nextSceneName != null)
                 {
                     //DialogTextManager.instance.DestroyObject();
                     sceneTransitionManager  = new SceneTransitionManager();
                     sceneTransitionManager.LoadTo(nextSceneName);
 
-            }
-            else { 
-                    DialogTextManager.instance.SetScenarios(new string[] { dataArray[index] });
-                    ++index;
+                // スクリプトをすべて読んだら && パタンが決めたら 
+                }else if (index > dataArray.Length - 1 && buttonOrContact != null)
+                {
+
+                }
+                else{ 
+                        DialogTextManager.instance.SetScenarios(new string[] { dataArray[index] });
+                        ++index;
                 }
             }
         
+    }
+    
+
+
+    public List<string> shuffleList(List<string> list)
+    {
+        System.Random rnd = new System.Random();
+
+        int n = list.Count;
+        while(n > 1)
+        {
+            n--;
+            int k = rnd.Next(n + 1);
+            string value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+        return list;
     }
 
 
