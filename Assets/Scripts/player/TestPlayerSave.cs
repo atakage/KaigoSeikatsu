d848122a17fe6769dd0c298ef736c4ca48dcc380
atakoko <atakoko2@gmail.com>
2020-11-25 22:48:01 +0900

    save load json

---
+++ Assets/Scripts/player/TestPlayerSave.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using LitJson;
+
+[System.Serializable]
+public class TestPlayerData
+{
+    public int money;
+    public int progress;
+
+    public TestPlayerData(int money, int progress)
+    {
+        this.money = money;
+        this.progress = progress;
+    }
+
+    public void printData()
+    {
+        Debug.Log("MONEY: " + money);
+        Debug.Log("PROGRESS: " + progress);
+    }
+}
+
+
+public class TestPlayerSave : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestPlayerData testPlayerData = new TestPlayerData(15000, 60);
+
+        JsonData dataJson = JsonMapper.ToJson(testPlayerData);
+
+        Debug.Log("SAVEDATA: " + dataJson.ToString());
+        File.WriteAllText(Application.dataPath + "/Resources/saveData/testPlayerData.json", dataJson.ToString());
+
+
+        string dataStr = File.ReadAllText(Application.dataPath + "/Resources/saveData/testPlayerData.json");
+
+        Debug.Log("LOADDATA: " + dataStr);
+
+        JsonData dataJsonCon = JsonMapper.ToObject(dataStr);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
