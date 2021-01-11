b04b23f9f77abc30b375069452d3fe528134fe02
atakoko <atakoko2@gmail.com>
2021-01-11 23:12:10 +0900

    morning event

--- Assets/Scripts/FacilityManager.cs
+++ Assets/Scripts/FacilityManager.cs
@@ -2,19 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class FacilityManager : MonoBehaviour
 {
     public PlayerSaveDataManager playerSaveDataManager;
+    public EventManager eventManager;
+    public ChatManager chatManager;
+    public string[] morningrequiredEvent = {"EV001","EV002","EV003"};
     // Start is called before the first frame update
     void Start()
     {
         playerSaveDataManager = new PlayerSaveDataManager();
+        eventManager = new EventManager();
+        chatManager = GameObject.Find("ChatManager").GetComponent("ChatManager") as ChatManager;
+
+        // ランダムで朝のイベント
+        string morningEventCode = CallMorningReqEvent(morningrequiredEvent);
+        EventListData[] loadedEventListData = playerSaveDataManager.LoadedEventListData();
+        EventListData eventItem = eventManager.FindEventByCode(loadedEventListData, morningEventCode);
+        List<string[]>scriptList = eventManager.ScriptSaveToList(eventItem);
+        chatManager.ShowDialogue(scriptList, morningEventCode);
+        
         PlayerData playerData = playerSaveDataManager.LoadPlayerData();
         Debug.Log("playerData: " + playerData.ToString());
 
         // UI setting
         GameObject.Find("Canvas").transform.Find("time").GetComponent<Text>().text = playerData.time;
         GameObject.Find("Canvas").transform.Find("fatigueBar").GetComponent<Slider>().value = playerData.fatigue;
+
+
+    }
+    
+    public string CallMorningReqEvent(string[] morningrequiredEvent)
+    {
+        System.Random random = new System.Random();
+        int randomIndex = random.Next(0, morningrequiredEvent.Length);
+        Debug.Log("Random morning EventCode: " + morningrequiredEvent[randomIndex]);
+        return morningrequiredEvent[randomIndex];
     }
 }
