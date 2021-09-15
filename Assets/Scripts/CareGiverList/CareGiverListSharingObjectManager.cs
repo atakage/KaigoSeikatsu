b078f8c7d337ff7ee014bc9d652da7d6af6920e9
atakoko <atakoko2@gmail.com>
2021-09-15 23:25:57 +0900

    care giver list player data ui set

---
+++ Assets/Scripts/CareGiverList/CareGiverListSharingObjectManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CareGiverListSharingObjectManager : MonoBehaviour
+{
+    public GameObject canvasGameObj;
+    public GameObject playerClearListBoxGameObj;
+    public GameObject playerClearScrollGameObj;
+    public GameObject playerClearScrollContainerGameObj;
+    public GameObject containerItem0GameObj;
+    // Start is called before the first frame update
+    // Awake() -> Start()
+    /*
+     * 
+     * 1. object disabled, script disabled
+     * Awake() X
+     * Start() X
+     * 
+     * 2. object enable, script disabled
+     * Awake() O
+     * Start() X
+     * 
+     * 3. object enable, script enable
+     * Awake() O
+     * Start() O
+     */
+
+    private void Awake()
+    {
+        this.canvasGameObj = GameObject.Find("Canvas");
+        this.playerClearListBoxGameObj = this.canvasGameObj.transform.Find("playerClearListBox").gameObject;
+        this.playerClearScrollGameObj = this.playerClearListBoxGameObj.transform.Find("playerClearScroll").gameObject;
+        this.playerClearScrollContainerGameObj = this.playerClearScrollGameObj.transform.Find("container").gameObject;
+        this.containerItem0GameObj = this.playerClearScrollContainerGameObj.transform.Find("item0").gameObject;
+
+    }
+}
---
+++ Assets/Scripts/CareGiverList/CareGiverListUIManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CareGiverListUIManager : MonoBehaviour
+{
+    public ClearData clearData;
+    public GameClearFileManager gameClearFileManager;
+    public CareGiverListSharingObjectManager careGiverListSharingObject;
+
+    private void Start()
+    {
+        gameClearFileManager = new GameClearFileManager();
+        careGiverListSharingObject = GameObject.Find("CareGiverListSharingObjectManager").GetComponent<CareGiverListSharingObjectManager>();
+
+        clearData = gameClearFileManager.LoadClearData();
+
+        // 最初プレイヤーデータをdefaultobjectにセット
+        InitPlayerClearData(clearData);
+
+        // プレイヤーデータが2つ以上ならオブジェクトを追加する
+    }
+
+    public void InitPlayerClearData(ClearData clearData)
+    {
+        // 一番目のデータを取り出す
+        PlayerData playerDataFirst = clearData.clearPlayerDataList[0];
+
+        // default objectに格納する
+        careGiverListSharingObject.containerItem0GameObj.transform.Find("upperBox").transform.Find("nameBox").transform.Find("value").GetComponent<Text>().text = playerDataFirst.name;
+        careGiverListSharingObject.containerItem0GameObj.transform.Find("upperBox").transform.Find("moneyBox").transform.Find("value").GetComponent<Text>().text = playerDataFirst.money;
+        careGiverListSharingObject.containerItem0GameObj.transform.Find("upperBox").transform.Find("satisfactionBox").transform.Find("value").GetComponent<Text>().text = playerDataFirst.satisfaction.ToString();
+        careGiverListSharingObject.containerItem0GameObj.transform.Find("lowerBox").transform.Find("playTimeBox").transform.Find("value").GetComponent<Text>().text = TimeSpan.FromSeconds(playerDataFirst.playTime).ToString("hh':'mm':'ss");
+        careGiverListSharingObject.containerItem0GameObj.transform.Find("lowerBox").transform.Find("endDateBox").transform.Find("value").GetComponent<Text>().text = playerDataFirst.endDate;
+    }
+}
