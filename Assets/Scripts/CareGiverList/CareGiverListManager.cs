d55b253c55071519120af3e4603a7096aae7847b
atakoko <atakoko2@gmail.com>
2021-09-23 23:09:54 +0900

    care giver list player data list

--- Assets/Scripts/CareGiverList/CareGiverListManager.cs
+++ Assets/Scripts/CareGiverList/CareGiverListManager.cs
@@ -1,8 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Diagnostics;
+using System;
 
 public class CareGiverListManager : MonoBehaviour
 {
+    public FirebaseManager firebaseManager;
+    public bool successSelectingPlayerDataList; // DBからプレイヤーデータリスト取り出しに成功
+    public bool actionFlagInUpdate; // Update()中である動作を指示するflag
 
+    private void Start()
+    {
+        actionFlagInUpdate = false;
+        firebaseManager = new FirebaseManager();
+        FireBaseConnection();
+    }
+
+    private void Update()
+    {
+        // DBからプレイヤーデータリストを取り出すとactionFlagInUpdate変更
+        if (successSelectingPlayerDataList) actionFlagInUpdate = true;
+
+        // プレイヤーデータリストの確保とactionFlagInUpdateがtrueなら
+        if (successSelectingPlayerDataList && actionFlagInUpdate)
+        {
+            UnityEngine.Debug.Log("delete block screen");
+            successSelectingPlayerDataList = false;
+            actionFlagInUpdate = false;
+        }
+
+    }
+
+    public async void FireBaseConnection()
+    {
+        // DB作業
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        bool connectionResult = false;
+        // 最大5秒Firebaseに接続を試みる
+        while (stopwatch.Elapsed < TimeSpan.FromMilliseconds(5000))
+        {
+            connectionResult = await firebaseManager.FireBaseConnection();
+            if (connectionResult) break;
+        }
+        UnityEngine.Debug.Log("completed connectionResult: " + connectionResult);
+        stopwatch = null;
+
+        // DBからプレイヤーデータリストを取り出す
+
+        // UI設定
+    }
 }
