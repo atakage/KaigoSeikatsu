765d7777ffdd4389464dbe084e84956b87a28c29
atakoko <atakoko2@gmail.com>
2021-10-03 19:53:36 +0900

    OnBeingDrag OnEndedDrag

---
+++ Assets/Scripts/CareGiverList/CareGiverListScrollRectManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+
+public class CareGiverListScrollRectManager : MonoBehaviour, IBeginDragHandler, IEndDragHandler
+{
+    public bool completedFadeOut;
+    public bool beingDrag;
+    public CareGiverListSharingObjectManager careGiverListSharingObjectManager;
+
+
+    private void Start()
+    {
+        careGiverListSharingObjectManager = GameObject.Find("CareGiverListSharingObjectManager").GetComponent<CareGiverListSharingObjectManager>();
+    }
+
+    public void OnEndDrag(PointerEventData data)
+    {
+        careGiverListSharingObjectManager.scrollHelpMessageBoxGameObj.SetActive(false);
+    }
+
+    public void OnBeginDrag(PointerEventData data)
+    {
+        careGiverListSharingObjectManager.scrollHelpMessageBoxGameObj.SetActive(true);
+    }
+
+    
+
+}
